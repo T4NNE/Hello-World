f5100f7586d61eea897d3b6d580bc160fc441933
T4NNE <tanmanip@gmail.com>
2025-02-01 13:11:22 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,32 @@
-Import java.until.Scammer;
+public class Main {
 
-class Demo
-{
-public static void main(String[]args)
-  {
-    Scanner myObj = new Scanner(System.in);
-    
-System.out.println("Hello World");
+    public static void main(String[] args) {
 
+        String name = "Tam";
+        String ausbildung = "Fachinformatoker für Anwendungsentwicklung";
+        String age = "20";
+
+        showGreeting();
+
+        System.out.println("Ich heiße " + name + ". Ich bewerbe mich als " + ausbildung + " und bin " + age + " Jahre alt.");
+    }
+
+    public static void showGreeting() {
+        Integer hour = 20;
+
+        if (hour < 9) {
+            System.out.println("Guten Morgen");
+        }
+        if (hour < 12 && hour > 9) {
+            System.out.println("Guten Tag");
+        }
+        if (hour < 18 && hour > 12) {
+            System.out.println("Guten Nachmitag");
+        }
+        if (hour > 18) {
+            System.out.println("Guten Abend");
+            {
+            }
+        }
     }
 }
